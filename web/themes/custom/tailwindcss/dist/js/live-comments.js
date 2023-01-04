bd41eef70f358c37ff74ce543e8c5f77b0a70d07
Andrew Brown <arbrown83@gmail.com>
2023-01-04 12:44:19 -0500

    Adding pusher JS listener to game nodes

---
+++ web/themes/custom/tailwindcss/dist/js/live-comments.js
@@ -0,0 +1,68 @@
+(function (Pusher, Drupal, drupalSettings) {
+    Drupal.behaviors.liveComments = {
+        attach: function (context) {
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
+            const nid = drupalSettings.sports_comments.nid;
+            console.log(drupalSettings.sports_comments.nid);
+            const serverUrl = "/",
+                comments = [],
+                pusher = new Pusher('c428b53f1cbc41e05ffe', {
+                    cluster: 'us2',
+                    encrypted: true
+                }),
+                // Subscribing to the 'sp0rts-comments' Channel
+                channel = pusher.subscribe('sp0rts-comments'),
+                commentForm = document.getElementById('comment-form'),
+                commentsList = document.getElementById('comments-list'),
+                playCommentTemplate = document.getElementById('play-comment-template');
+
+            // Binding to Pusher Event on our 'sp0rts-comments' Channel
+            let pusherEvent = 'new_play_' + nid;
+            channel.bind(pusherEvent, newCommentReceived);
+
+            // Adding to Comment Form Submit Event
+            // commentForm.addEventListener("submit", addNewComment);
+
+            // New Comment Receive Event Handler
+            // We will take the Comment Template, replace placeholders & append to commentsList
+            function newCommentReceived(data) {
+                // If data has a play_title property, this is a play comment.
+                if (data.play_title) {
+                    let newCommentHtml = playCommentTemplate.innerHTML.replace('{{ play_title }}', data.play_title);
+                    newCommentHtml = newCommentHtml.replace('{{ play_quarter }}', data.play_quarter);
+                    newCommentHtml = newCommentHtml.replace('{{ play_time }}', data.play_time);
+                    newCommentHtml = newCommentHtml.replace('{{ play_t1_logo }}', data.play_t1_logo);
+                    newCommentHtml = newCommentHtml.replace('{{ play_t2_logo }}', data.play_t2_logo);
+                    newCommentHtml = newCommentHtml.replace('{{ play_score }}', data.play_score);
+                    newCommentHtml = newCommentHtml.replace('{{ play_down }}', data.play_down);
+                    newCommentHtml = newCommentHtml.replace('{{ play_distance }}', data.play_distance);
+                    const newCommentNode = document.createElement('div');
+                    newCommentNode.classList.add('comment');
+                    newCommentNode.innerHTML = newCommentHtml;
+                    commentsList.appendChild(newCommentNode);
+                }
+            }
+
+            function addNewComment(event) {
+                event.preventDefault();
+                const newComment = {
+                    "name": document.getElementById('new_comment_name').value,
+                    "comment": document.getElementById('new_comment_text').value
+                };
+
+                const xhr = new XMLHttpRequest();
+                xhr.open("POST", serverUrl + "comment", true);
+                xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState !== 4 || xhr.status !== 200) return;
+
+                    // On Success of creating a new Comment
+                    console.log("Success: " + xhr.responseText);
+                    commentForm.reset();
+                };
+                xhr.send(JSON.stringify(newComment));
+            }
+        }
+    }
+})(Pusher, Drupal, drupalSettings);
