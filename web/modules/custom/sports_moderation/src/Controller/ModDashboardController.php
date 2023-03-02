d8763b1874ddc4ba96f344071c34f6526a502015
Andrew Brown <arbrown83@gmail.com>
2023-03-01 19:44:17 -0500

    Creating a better mod dashboard.

--- web/modules/custom/sports_moderation/src/Controller/ModDashboardController.php
+++ web/modules/custom/sports_moderation/src/Controller/ModDashboardController.php
@@ -3,9 +3,7 @@
 namespace Drupal\sports_moderation\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\group\Entity\Group;
-use Drupal\user\Entity\User;
 
 /**
  * Display a dashboard to moderators.
@@ -13,9 +11,33 @@ use Drupal\user\Entity\User;
 class ModDashboardController extends ControllerBase {
 
   function build() {
-    $text = 'Welcome to your Moderator Dashboard.';
+    // Check if current user is group moderator.
+    $groups = \Drupal::service('group.membership_loader')->loadByUser();
+    $mod_groups = [];
+    if ($groups) {
+      // Check all user groups to see if they're a mod of any of them.
+      foreach ($groups as $membership) {
+        $gid = $membership->getGroup()->id();
+        $group = Group::load($gid);
+        $mod_roles = [
+          $group->bundle() . '-moderator',
+          $group->bundle() . '-admin'
+        ];
+        // Get this group's moderators/admins.
+        $mods = $group->getMembers($mod_roles);
+        foreach ($mods as $mod) {
+          $id = $mod->getUser()->id();
+          // If the mod ID matches the current user, add this group to the list.
+          if ($id == $this->currentUser()->id()) {
+            $mod_groups[$group->id()] = $group->label();
+          }
+        }
+      }
+    }
+    // Send all group IDs that the user moderates to build views in twig.
     return [
-      '#markup' => $this->t($text),
+      '#theme' => 'sports_moderation_dashboard',
+      '#groups' => $mod_groups
     ];
   }
 }
